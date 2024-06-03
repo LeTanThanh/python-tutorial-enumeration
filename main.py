8a2dd745bcc5cb326dd2835ba904a5e308037127
TanThanhLe <tanthanhcr95@gmail.com>
2024-06-03 23:18:33 +0700

    Introduction to the Python Enumeration

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from color import Color
+
+# Python Enumeration
+
+if __name__ == "__main__":
+  ## Introduction to the Python Enumeration
+
+  print(type(Color.RED))              # <enum "Color">
+  print(isinstance(Color.RED, Color)) # True
+  print(Color.RED.name)               # RED
+  print(Color.RED.value)              # 1
